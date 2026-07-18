17922cbd2d5ec85f1d330f1955bd5760f78b9961
officialsadiah <sadia.siddiqie33@gmail.com>
2026-07-18 19:00:25 +0600

    java script of first player is added

---
+++ javascript.js
@@ -0,0 +1,25 @@
+let headingOne = document.querySelector(".headingOne")
+let inputOne = document.querySelector(".inputOne")
+let buttonOne = document.querySelector(".buttonOne")
+let erroeOne = document.querySelector(".erroeOne")
+
+buttonOne.addEventListener("click" , function(){
+// console.log(headingOne)
+// console.log(inputOne)
+// console.log(buttonOne)
+
+
+if(!inputOne.value){
+    erroeOne.innerHTML = "Please Enter A value"
+    
+    
+}else if(isNaN(inputOne.value)){
+    erroeOne.innerHTML = "Please Enter A Number"
+}
+else if(inputOne.value>10 || inputOne.value<1){
+    erroeOne.innerHTML = "Please Enter a number betweem 1 - 10"
+}
+else{
+    erroeOne.innerHTML = "game started"
+}
+})
